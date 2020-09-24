4bf2db04ddd2e14068586473059fe5e2442b936e
t.watanabe <t.watanabe@ifcreate.co.jp>
2020-09-24 17:45:20 +0900

    動的importの例

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from './icon.svg';
-import Data from './data.xml';
-import printMe from './print.js';
-
-function component() {
+async function getComponent() {
   const element = document.createElement('div');
-  const btn = document.createElement('button');
+  const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
 
   return element;
 }
 
-document.body.appendChild(component());
+getComponent().then(component => {
+  document.body.appendChild(component);
+});
