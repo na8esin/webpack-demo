124afde85b358fb57a193712d3f5572990449db1
t.watanabe <t.watanabe@ifcreate.co.jp>
2020-09-24 15:25:21 +0900

    xmlの読み込み. print.jsの追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js',
+    },
     devServer: {
         contentBase: './dist',
     },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
@@ -28,6 +31,18 @@ module.exports = {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [
                     'file-loader',
+                ],
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader',
+                ],
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader',
                 ],
             },
         ],
